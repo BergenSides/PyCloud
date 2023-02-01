7537b36fcf4279281048329566d8563d3df3b0ad
robotkid828 <122555673+BergenSides@users.noreply.github.com>
2023-02-01 08:37:35 -0500

    Update PyCloud.py

--- PyCloud.py
+++ PyCloud.py
@@ -1,4 +1,5 @@
 import os, json, websocket, requests
+ID = ""
 
 def SendPacket(Packet):
   Websocket.send(f"{json.dumps(Packet)}\n")
@@ -8,7 +9,12 @@ def SetupConnection(Username, ProjectID, Cookie):
   global ID
   global Websocket
   User = Username
-  ID = str(ProjectID)
+  if str(type(ProjectID)) == "<class 'dict'>":
+    ID = []
+    for x in range(len(ProjectID)):
+      ID.append(ProjectID[x])
+  else:
+    ID = str(ProjectID)
   Websocket = websocket.WebSocket()
   Websocket.connect(
     "wss://clouddata.scratch.mit.edu",
@@ -16,25 +22,37 @@ def SetupConnection(Username, ProjectID, Cookie):
     origin="https://scratch.mit.edu",
     enable_multithread=True
   )
-  SendPacket({
-    "method": "handshake",
-    "user": Username,
-    "project_id": str(ProjectID)
-  })
-
-def SetCloudVar(CloudName, Value):
+  if str(type(ProjectID)) == "<class 'dict'>":
+    for x in range(len(ID)):
+      SendPacket({
+        "method": "handshake",
+        "user": Username,
+        "project_id": ID[x]
+      })
+  else:
+    SendPacket({
+      "method": "handshake",
+      "user": Username,
+      "project_id": ID
+    })
+  
+def SetCloudVar(CloudName, Value, ProjectID=""):
+  if ProjectID == "":
+    ProjectID = ID
   SendPacket({
   "method": "set",
   "name": f"☁ {CloudName}",
   "value": str(Value),
   "user": User,
-  "project_id": ID,
+  "project_id": ProjectID,
   })
 
-def GetCloudVar(CloudName):
+def GetCloudVar(CloudName, ProjectID=""):
   Limit = 100
+  if ProjectID == "":
+    ProjectID = ID
   while True:
-    Response = requests.get(f"https://clouddata.scratch.mit.edu/logs?projectid={ID}&limit={Limit}&offset=0").json()
+    Response = requests.get(f"https://clouddata.scratch.mit.edu/logs?projectid={ProjectID}&limit={Limit}&offset=0").json()
     for x in range(100):
       if Response[x + Limit - 100]["name"] == f"☁ {CloudName}":
         return Response[x + Limit - 100]["value"]
